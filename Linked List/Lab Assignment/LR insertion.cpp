e02f37a4a2720f9678d8018fc790b9c48098be39
Tahmid Hossain Chowdhury Mahin <chowdhurytahmid20@gmail.com>
2025-01-14 11:35:42 +0600

    Create LR insertion.cpp

---
+++ Linked List/Lab Assignment/LR insertion.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Node{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int n){
+        data=n;
+        next=nullptr;
+        prev=nullptr;
+    }
+};
+
+
+using namespace std;
+
+int main()
+{
+    Node *n=new Node(0);
+    Node *nn;
+    Node *head=n;
+    Node *cur=n;
+    int si;
+    cin>>si;
+    string s;
+    cin>>s;
+    for(int i=0;i<si;i++){
+        if(s[i]=='L'){
+            nn=new Node(i+1);
+            nn->next=cur;
+            nn->prev=cur->prev;
+            if(cur->prev!=nullptr) cur->prev->next=nn;
+            cur->prev=nn;
+            if(cur==head) head=cur->prev;
+            cur=cur->prev;
+
+        }else{
+            nn=new Node(i+1);
+            nn->next=cur->next;
+            nn->prev=cur;
+            if(cur->next!=nullptr) cur->next->prev=nn;
+            cur->next=nn;
+            cur=cur->next;
+        }
+
+    }
+    cur=head;
+    while(cur!=nullptr){
+        cout<<cur->data<<" ";
+        cur=cur->next;
+    }
+    return 0;
+}
